b3f63f4ec4883197e736a1bbf96cbfd7d78f4210
migmogcam <junior7395@hotmail.es>
2020-03-10 00:31:48 +0100

    index page

---
+++ src/main/java/app/fastyleApplication/fastyle/controller/IndexController.java
@@ -0,0 +1,15 @@
+package app.fastyleApplication.fastyle.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@GetMapping("/")
+    public String main(Model model) {
+        return "index"; //view
+    }
+
+}
